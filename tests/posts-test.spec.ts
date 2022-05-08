6598020abc23fc43f4572fa596184ccdcd1c2f42
PereAnd <aa.pereza1@uniandes.edu.co>
2022-05-07 20:32:15 -0500

    Empezando a hacer las pruebas desde cero

--- tests/posts-test.spec.ts
+++ tests/posts-test.spec.ts
@@ -14,27 +14,25 @@ test.beforeEach(async ({ page }) => {
 });
 
 test.describe('Crear, editar, despublicar, eliminar posts', () => {
-    test('Entrar al login', async({ page }) =>{
+    test('Entrar al login', async ({ page }) => {
         // Ingresar Email
-        await page.locator('#ember7').fill(credentials.email);
+        await page.locator('input[name=identification]').fill(credentials.email);
         // Ingresar Password
-        await page.locator('#ember9').fill(credentials.pass);
+        await page.locator('input[name=password]').fill(credentials.pass);
         // Clic en Sign in
         await Promise.all([
-        page.waitForNavigation(/*{ url: 'http://localhost:2368/ghost/#/dashboard' }*/),
-        page.locator('#ember11').click(),
-        await expect(page).toHaveURL('http://localhost:2368/ghost/#/dashboard'),
-        await page.screenshot({ path: 'screenshots/Entrar al login.png', fullPage: true }),
+            page.waitForNavigation( /*{ url: 'http://localhost:2368/ghost/#/dashboard' }*/),
+            page.locator('button[type=submit]').click(),
+            await expect(page).toHaveURL('http://localhost:2368/ghost/#/dashboard'),
+            await page.screenshot({ path: 'screenshots/Entrar al login.png', fullPage: true }),
         ]);
 
 
 
 
         // Click Posts
-        await Promise.all([
-        page.waitForNavigation(/*{ url: 'http://localhost:2368/ghost/#/posts' }*/),
-        page.locator('#ember26').click()
-        ]);
+        await page.locator('li', { hasText: 'Posts' }).click();
+        
         // Click text=New post
         await page.locator('text=New post').click();
         await expect(page).toHaveURL('http://localhost:2368/ghost/#/editor/post');
@@ -42,8 +40,8 @@ test.describe('Crear, editar, despublicar, eliminar posts', () => {
         await page.locator('textarea').fill(nombrePostPrueba);
         // Esperar navegación
         await Promise.all([
-        page.waitForNavigation(/*{ url: 'http://localhost:2368/ghost/#/editor/post/6276ad360569ae06343c1dfc' }*/),
-        page.locator('.koenig-editor__editor').click()
+            page.waitForNavigation(),
+            page.locator('.koenig-editor__editor').click()
         ]);
         // Click Publish
         await page.locator('div[role="button"]:has-text("Publish")').click();
@@ -53,36 +51,41 @@ test.describe('Crear, editar, despublicar, eliminar posts', () => {
         await page.locator('button:has-text("Publish")').click(),
         // Captura de pantalla
         await Promise.all([
-            page.waitForNavigation(/*{ url: 'http://localhost:2368/ghost/#/dashboard' }*/),
-            await page.goto('http://localhost:2368/ghost/#/dashboard'),
-            await page.screenshot({ path: 'screenshots/Nuevo post publicando.png', fullPage: true })
+            page.waitForNavigation(),
+            // Click Posts
+            await page.locator('a span', { hasText: 'Posts'}).click(),
+            page.waitForNavigation(),
+            await page.locator('a[title=Published]').click(),
+            page.waitForNavigation(),
+            await page.screenshot({ path: 'screenshots/Nuevo post publicado.png', fullPage: true }),
+            
         ]);
 
 
 
+        // Editar POST
+        await page.locator('h2', { hasText: 'Posts' }).click();
+        page.waitForNavigation(),
+        await page.pause()
+        await page.locator('h3:has-text("nuevoPost")').click();
+        await page.locator('div p').fill('----Se añade descripcion del post----');
 
-        await Promise.all([
-            page.waitForNavigation(/*{ url: 'http://localhost:2368/ghost/#/posts' }*/),
-            page.locator('#ember26').click()
-            ]);
-        // Click text=Otro Post
-        await page.locator('text=NuevoPost').click();
         // Click textarea
-        await page.locator('textarea').click();
         // Fill textarea
-        await page.locator('textarea').fill('----Se añade descripcion del post----');
         // Click div[role="button"]:has-text("Update")
         await page.locator('div[role="button"]:has-text("Update")').click();
         // Click button:has-text("Update")
         await page.locator('button:has-text("Update")').click();
         // Captura de pantalla
         await Promise.all([
-            page.waitForNavigation(/*{ url: 'http://localhost:2368/ghost/#/dashboard' }*/),
-            await page.goto('http://localhost:2368/ghost/#/dashboard'),
-            await page.screenshot({ path: 'screenshots/Post editado.png', fullPage: true })
+            page.waitForNavigation(),
+            // Click Posts
+            await page.locator('a span', { hasText: 'Posts'}).click(),
+            page.waitForNavigation(),
+            await page.locator('a[title=Published]').click(),
+            await page.screenshot({ path: 'screenshots/Nuevo post publicado.png', fullPage: true }),
+            await page.pause()
         ]);
-
-
     })
 
     // test('Crear nuevo post', async({ page }) => {
