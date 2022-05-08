2affc88150281ec3785a6b8ed0a262d54875cfd5
“j.cardonao” <j.cardonao@uniandes.edu.co>
2022-05-08 11:33:58 -0500

    Escenario 13: Cambiar el sitio a visibilidad privada - playwright

---
+++ tests/13_Cambiar_sitio_privado.spec.ts
@@ -0,0 +1,37 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+
+  // Go to http://localhost:2368/ghost/#/signin
+  await page.goto('http://localhost:2368/ghost/#/signin');
+
+  // Click [placeholder="jamie\@example\.com"]
+  await page.locator('[placeholder="jamie\\@example\\.com"]').click();
+
+  // Fill [placeholder="jamie\@example\.com"]
+  await page.locator('[placeholder="jamie\\@example\\.com"]').fill('Jorge.cardonaor@gmail.com');
+
+  // Click [placeholder="•••••••••••••••"]
+  await page.locator('[placeholder="•••••••••••••••"]').click();
+
+  // Fill [placeholder="•••••••••••••••"]
+  await page.locator('[placeholder="•••••••••••••••"]').fill('0123456789');
+
+  // Click button:has-text("Sign in →")
+  await Promise.all([
+    page.waitForNavigation(/*{ url: 'http://localhost:2368/ghost/#/dashboard' }*/),
+    page.locator('button:has-text("Sign in →")').click()
+  ]);
+
+  // Click label span
+  await page.locator('label span').click();
+
+  // Click text=Advanced settings Make this site private Enable protection with a simple shared  >> span
+  await page.locator('text=Advanced settings Make this site private Enable protection with a simple shared  >> span').click();
+
+  // Click button:has-text("Save")
+  await page.locator('button:has-text("Save")').click();
+
+  await page.screenshot({ path: './screenshots/cambiar_sitio.png', fullPage: true });
+
+});
